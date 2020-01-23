e1ab506b403d6a6be43482af7a0233859b7aa7ea
Edwin Marte Torres <kdgpro15@gmail.com>
2020-01-23 14:05:26 -0400

    230112003 - Creating the templates to be used in the project

---
+++ lcrud/lblog/resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© {{ date('Y') }} Ez-Learning BD:
+        <a href=""> ez-learningbd.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
