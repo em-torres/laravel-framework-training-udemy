4d9975e4c129e05bacc6d1e8356e894408ca0a79
Edwin Marte Torres <kdgpro15@gmail.com>
2020-02-01 20:36:51 -0400

    010220003 - Creating Views and routes to update passwords

--- laut/resources/views/layouts/app.blade.php
+++ laut/resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('password.change') }}">
+                                        {{ __('Change Password') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
